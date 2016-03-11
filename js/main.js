9fb5eae2a3cc0827673bf7dbdc5f2efc6eb0122a
Marissa96 <marissa.boswall96@gmail.com>
2016-03-11 16:06:14 -0500

    added new h1s and us

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var $newH11 = $('<h1>');
+var $newUl1 = $('<ul>');
+var $newH12 = $('<h1>');
+var $newUl2 = $('<ul>');
+
+$newH11.html('Below ground veggies');
+$('body').append($newH11);
+$('body').append($newUl1);
+
+$newH12.html('Above ground veggies');
+$('body').append($newH12);
+$('body').append($newUl2);
